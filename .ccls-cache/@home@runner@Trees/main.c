8db0cdeb18eaa6690082626ad8a921718ebdeedd
Abhijit B <7652423-AbhijitB2@users.noreply.replit.com>
2024-08-08 20:21:22 +0000

    trying kth sum, do not merge

--- .ccls-cache/@home@runner@Trees/main.c
+++ .ccls-cache/@home@runner@Trees/main.c
@@ -9,14 +9,13 @@ struct node *insert(struct node *root, int val){
   struct node *newNode;
   newNode = (struct node *)malloc(sizeof(struct node));
   newNode->val = val;
+  newNode->left = NULL;
+  newNode->right = NULL;
   if(root==NULL){
-    root = newNode;
-    newNode->left = NULL;
-    newNode->right = NULL;
     return newNode;
   }
   else{
-    if(newNode->val>val){
+    if(root->val>val){
       root->left = insert(root->left, val);
     }
     else{
@@ -32,10 +31,50 @@ void inorder(struct node *root){
     inorder(root->right);
   }
 }
+// struct node *smallestNode(struct node *root){
+//   if(root== NULL || root->left==NULL){
+//     return root;
+//   }
+//   else{
+//     return smallestNode(root->left);
+//   }
+// }
+// struct node *kthsmallestElements(struct node *root){
+//   int count;
+//   count = 0;
+//   struct node *ptr;
+//   if(root==NULL){
+//     return NULL;
+//   }
+//   else{
+//     ptr = root;
+//     while(ptr->left!=NULL){
+//       ptr = ptr->left;
+//     }
+//   }
+//   return root;
+// }
+
+struct node *smallest(struct node *root){
+  struct node *ptr;
+  if(root==NULL){
+    return NULL;
+  }
+  else{
+    ptr = root;
+    while(ptr->left!=NULL){
+      ptr = ptr->left;
+    }
+  }
+  
+}
 int main(){
   struct node *root = NULL;
+  struct node *root1 = NULL;
   root = insert(root, 5);
   root = insert(root, 4);
   root = insert(root, 11);
+  root = insert(root, 1);
   inorder(root);
+  printf("\n%d-->\n", smallest(root)->val);
 }
